dce660727ef24688904ca8ac5564046c4d2efc0c
zdraquel <143689837+zdraquel@users.noreply.github.com>
2023-09-05 14:49:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function addition(){
+    a=document.getElementById("number1").value
+    b=document.getElementById("number2").value
+    c=a+b
+    document.getElementById("outputbox").innerHTML=c
+}
+function subract(){
+    a=document.getElementById("number1").value
+    b=document.getElementById("number2").value
+    c=a-b
+    document.getElementById("outputbox").innerHTML=c
+}
+function multipy(){
+    a=document.getElementById("number1").value
+    b=document.getElementById("number2").value
+    c=a*b
+    document.getElementById("outputbox").innerHTML=c
+}
+function division(){
+    a=document.getElementById("number1").value
+    b=document.getElementById("number2").value
+    c=a/b
+    document.getElementById("outputbox").innerHTML=c
+}
